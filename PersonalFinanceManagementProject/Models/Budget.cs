4477d14af20a48affb99fc122182d2efd0cd4983
Andrew <andrewozo123@gmail.com>
2024-02-22 13:50:12 -0500

    Created Transaction Model and Budget Model

---
+++ PersonalFinanceManagementProject/Models/Budget.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalFinanceManagementProject.Models
+{
+    public class Budget
+    {
+        public int Id { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
--- PersonalFinanceManagementProject/Models/Transaction.cs
+++ PersonalFinanceManagementProject/Models/Transaction.cs
@@ -2,6 +2,7 @@
 {
     public class Transaction
     {
+        public int Id { get; set; }
         public decimal Amount { get; set; }
 
         public DateTime? Date { get; set; }
