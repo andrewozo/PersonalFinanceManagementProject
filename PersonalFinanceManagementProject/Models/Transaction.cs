1f41143ab8e9e43a87cc4380445c412d4d22dfef
Andrew <andrewozo123@gmail.com>
2024-02-22 13:21:21 -0500

    Add project files.

---
+++ PersonalFinanceManagementProject/Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalFinanceManagementProject.Models
+{
+    public class Transaction
+    {
+        public decimal Amount { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public string Category { get; set; } = String.Empty;
+    }
+}
