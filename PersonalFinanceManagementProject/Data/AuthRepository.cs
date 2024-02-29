8730d43f61d487402406e0b206163cbdc58cdaf0
Andrew <andrewozo123@gmail.com>
2024-02-29 15:59:12 -0500

    Created functions to hash and salt passwords

--- PersonalFinanceManagementProject/Data/AuthRepository.cs
+++ PersonalFinanceManagementProject/Data/AuthRepository.cs
@@ -1,8 +1,24 @@
 ﻿
+using AutoMapper;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
 namespace PersonalFinanceManagementProject.Data
 {
     public class AuthRepository : IAuthRepository
     {
+
+        private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+
+        public AuthRepository(DataContext context, IMapper mapper, IConfiguration configuration)
+        {
+            _context = context;
+            _mapper = mapper;
+            _configuration = configuration;
+        }
         public Task<ServiceResponse<string>> Login(string username, string password)
         {
             throw new NotImplementedException();
@@ -16,6 +32,57 @@ namespace PersonalFinanceManagementProject.Data
         public Task<bool> UserExists(string username)
         {
             throw new NotImplementedException();
+        }
+
+
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password,  byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+            };
+
+            var appSettingsToken = _configuration.GetSection("AppSettings:Token").Value;
+
+            if (appSettingsToken == null)
+            {
+                throw new Exception("AppSettings token is null");
+            }
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(appSettingsToken));
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var TokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token = tokenHandler.CreateToken(TokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
