e523747b7e2262c65b3067932bb6a52e3c69844c
Andrew <andrewozo123@gmail.com>
2024-02-29 16:44:03 -0500

    Added Transaction Controller

---
+++ PersonalFinanceManagementProject/Controllers/TransactionController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonalFinanceManagementProject.DTOS.Transaction;
+using PersonalFinanceManagementProject.Services;
+
+namespace PersonalFinanceManagementProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<ServiceResponse<GetTransactionDto>>> GetAllTransactions()
+        {
+            return Ok(await _transactionService.GetAllTransactions());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetTransactionDto>>> GetSingleTransaction(int id)
+        {
+            return Ok(await _transactionService.GetTransactionById(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<GetTransactionDto>>>> AddTransaction(AddTransactionDto transaction)
+        {
+            return Ok(await _transactionService.AddTransaction(transaction));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<List<GetTransactionDto>>>> UpdateTransaction(UpdateTransactionDto updateTransaction)
+        {
+            var response = await _transactionService.UpdateTransaction(updateTransaction);
+
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult<ServiceResponse<GetTransactionDto>>> DeleteTransaction(int id)
+        {
+            var response = await _transactionService.DeleteTransaction(id);
+
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(Response);
+        }
+
+
+
+
+
+
+    }
+}
