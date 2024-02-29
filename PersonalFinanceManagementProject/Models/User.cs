c33887bfa59f0bdcddc3b7afad33bff4a9dc90d2
Andrew <andrewozo123@gmail.com>
2024-02-29 15:39:51 -0500

    Tier 2, created User model and Auth Repository

---
+++ PersonalFinanceManagementProject/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonalFinanceManagementProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public String Username { get; set; } = string.Empty;
+
+        public byte[] PasswrodHash { get; set; } = new byte[0];
+
+        public byte[] PasswordSalt { get; set; } = new byte[0]; 
+    }
+}
