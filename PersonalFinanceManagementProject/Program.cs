0e641b41e6685a5e94dcdf45fda475e489f30ca9
Andrew <andrewozo123@gmail.com>
2024-08-12 22:16:07 -0400

    Changes

--- PersonalFinanceManagementProject/Program.cs
+++ PersonalFinanceManagementProject/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.OpenApi.Models;
 using Auth0.AspNetCore.Authentication;
 using PersonalFinanceManagementProject.Services;
 
-//testing
+//testing 123
 
 var builder = WebApplication.CreateBuilder(args);
 
